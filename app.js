71c694539ec5a7ed54c4b1b5c7b9a7f7569ad2fb
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-29 13:14:30 +0200

    "Added geolocation functionality, refactored weather data rendering, and updated CSS styles"

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ let url; // api request
 let units = "metric"; // Fahrenheit - imperial
 let lang = "en"; // for Deutsch DE
 let cities = [];
-let userLocation = false;
 
 //! Date Set
 
@@ -59,33 +58,43 @@ form.addEventListener("submit", (e) => {
   form.reset();
 });
 
+locate.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(({ coords }) => {
+    //console.log(coords);
+    const { latitude, longitude } = coords;
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&lang=${lang}&appid=${apiKey}`;
+    getWeatherData();
+  });
+});
+
 const getWeatherData = async () => {
-  const { data } = await axios(url);
-  const { main, name, sys, weather } = data;
-  console.log(data);
-  // const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`; //^ alternatif
-  const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`; //^ openweathermap.org
+  try {
+    const { data } = await axios(url);
+    const { main, name, sys, weather } = data;
+    // const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`; //^ alternatif
+    const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`; //^ openweathermap.org
 
-  const card = document.createElement("div");
-  card.classList.add("col");
-  card.setAttribute("id", `${name}`);
-  card.innerHTML = `
-  <div class="weather-widget">
-  <div class="weather-header">
-    <p>${formattedDate}</p>
-    <h2>${name}, ${sys.country}</h2>
-  </div>
-  <div class="weather-body">
-    <div class="weather-icon">
-      <img src="${iconUrl}" alt="weather icon" />
+    const card = document.createElement("div");
+    card.classList.add("col");
+    card.setAttribute("id", `${name}`);
+    card.innerHTML = `
+    <div class="weather-widget card mb-4  ">
+    <div class="weather-header">
+      <p>${formattedDate}</p>
+      <h2>${name}, ${sys.country}</h2>
     </div>
-    <div class="temperature">
-    <p>${weather[0].description}</p>
-      <h1>${main.temp.toFixed(0)} °C </h1>
+    <div class="weather-body">
+      <div class="weather-icon">
+        <img src="${iconUrl}" alt="weather icon" />
+      </div>
+      <div class="temperature">
+      <p>${weather[0].description}</p>
+        <h1>${main.temp.toFixed(0)} °C </h1>
+      </div>
     </div>
-  </div>
-</div> 
-  `;
+  </div> 
+    `;
 
-  cardContainer.appendChild(card);
+    cardContainer.prepend(card);
+  } catch (error) {}
 };
