5992877552a3af8d34eecd8f6bcac7d4850cf884
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-29 12:54:55 +0200

    "Added JavaScript code for weather app functionality and updated CSS styles for weather widget"

---
+++ app.js
@@ -0,0 +1,58 @@
+const form = document.querySelector("form");
+const input = document.querySelector("form input.form-control");
+const cardContainer = document.getElementById("card-container");
+const alertMessage = document.getElementById("alert");
+const locate = document.getElementById("locate");
+const locationDiv = document.getElementById("userLocation");
+const langButton = document.querySelector(".language");
+
+//! Variables
+let apiKey = "75b251ce9d3d5c7bf9e4f1832b237076";
+let url; // api request
+let units = "metric"; // Fahrenheit - imperial
+let lang = "en"; // for Deutsch DE
+let cities = [];
+let userLocation = false;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input.value) {
+    const city = input.value;
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&lang=${lang}&appid=${apiKey}`;
+
+    getWeatherData();
+  }
+  form.reset();
+});
+
+const getWeatherData = async () => {
+  const { data } = await axios(url);
+  const { main, name, sys, weather } = data;
+  console.log(data);
+  // const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`; //^ alternatif
+  const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`; //^ openweathermap.org
+
+  const card = document.createElement("div");
+  card.classList.add("col");
+  card.setAttribute("id", `${name}`);
+  card.innerHTML = `
+  <div class="weather-widget">
+  <div class="weather-header">
+    <p></p>
+    <h2>${name}, ${sys.country}</h2>
+  </div>
+  <div class="weather-body">
+    <div class="weather-icon">
+      <img src="${iconUrl}" alt="weather icon" />
+    </div>
+    <div class="temperature">
+    <p>${weather[0].description}</p>
+      <h1>${main.temp.toFixed(0)} °C </h1>
+    </div>
+  </div>
+</div> 
+  `;
+
+  cardContainer.appendChild(card);
+};
